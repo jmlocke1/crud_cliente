34cbf11bc3b33e0070c8e01aa71774f35295c984
Josemi Xeon <worldcode.info@gmail.com>
2023-11-10 19:57:31 +0100

    Recomenzando el proyecto con renderizado en el cliente

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Aplicación CRUD con Angular y MongoDB en el backend. Parte cliente
 
-# CrudCliente
+# Cliente
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
 
@@ -27,5 +27,3 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-# crud_cliente
-
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'crud_cliente' title`, () => {
+  it(`should have the 'cliente' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud_cliente');
+    expect(app.title).toEqual('cliente');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud_cliente');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cliente');
   });
 });
