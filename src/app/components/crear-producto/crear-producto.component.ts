9dd3e3237e8201fe46a24fbd327d5396a23fe54d
Josemi Xeon <worldcode.info@gmail.com>
2023-11-13 00:07:45 +0100

    Crear producto, formulario reactivo

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
 	selector: 'app-crear-producto',
 	standalone: true,
 	imports: [
 		CommonModule,
-		RouterModule
+		RouterModule,
+		ReactiveFormsModule
 	],
 	templateUrl: './crear-producto.component.html',
 	styleUrl: './crear-producto.component.css'
 })
-export class CrearProductoComponent {
+export class CrearProductoComponent implements OnInit {
+	productoForm: FormGroup;
 
+	constructor(private fb: FormBuilder) {
+		this.productoForm = this.fb.group({
+			producto: ['', Validators.required],
+			categoria: ['', Validators.required],
+			ubicacion: ['', Validators.required],
+			precio: ['', Validators.required]
+		});
+	}
+
+	ngOnInit(): void {
+		
+	}
+
+	agregarProducto() {
+		console.log(this.productoForm);
+	}
 }
